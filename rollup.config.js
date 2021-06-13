507181adf28118386ff18d72e0a9b2cae52059e5
CaoMeiYouRen <996881204@qq.com>
2021-06-14 00:33:21 +0800

    fix: 回退 lodash 的改动

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import json from '@rollup/plugin-json'
 import analyzer from 'rollup-plugin-analyzer'
-import { upperFirst, camelCase } from 'lodash-es'
+import { upperFirst, camelCase } from 'lodash'
 import { dependencies, peerDependencies, name } from './package.json'
 const external = Object.keys({ ...dependencies, ...peerDependencies }) // 默认不打包 dependencies, peerDependencies
 const outputName = upperFirst(camelCase(name))// 导出的模块名称 PascalCase
